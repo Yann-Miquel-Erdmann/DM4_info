b4d088f356e1a848f589ca8e810a66ee73827d63
Falaux-Bachelot Erwan <erwan.faba@gmail.com>
2024-05-04 08:16:07 +0200

    first file

---
+++ satsolver.ml
@@ -0,0 +1,13 @@
+let test () =
+  assert (1 = 1);
+  print_string "tous les tests ont réussi\n"
+;;
+
+let main () =
+  test ();
+  print_int (Array.length Sys.argv); print_string "\n\n";
+  print_string Sys.argv.(0); print_string "\n\n";
+  print_string Sys.argv.(1); print_string "\n\n";
+;;
+
+let _ = main()
